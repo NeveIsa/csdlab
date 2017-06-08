cb07e050c63dde060e7efaac7cf765b3ffae9b5b
Tarun Choubisa <choubisa.tarun@gmail.com>
2017-06-09 02:20:11 +0530

    latest tools/fifo.py

--- tools/fifo.py
+++ tools/fifo.py
@@ -1,20 +1,21 @@
 import socket,serial
-import sys
+import sys,subprocess
 
-if len(sys.argv)>1:
-  ttyport=sys.argv[1]
-else:
-  ttyport="/dev/ttyUSB1"
 
-ser=serial.Serial(ttyport,115200)
+ttyports=subprocess.check_output("ls /dev/ttyUSB*",shell=1)
+ttyports=ttyports.split('\n')[:-1]
+ttyportTX=ttyports[0]
+
+
+print "Connect RX to port:",ttyports[1],"and hit RETURN"
+raw_input()
+
+ser=serial.Serial(ttyportTX,115200)
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 s.connect(("10.208.20.42",9090))
 
-
-print "Connect /dev/ttyUSB0/x and hit RETURN..."
-raw_input()
 
 while 1:
   rx=s.recv(200)
